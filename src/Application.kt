5d76eb4c584f2bfe5d2df0ea156a5fd16840dcf7
Rubén M <ruben.mendez@gmail.com>
2020-10-12 18:24:23 +0200

    Clean up project

--- src/Application.kt
+++ src/Application.kt
@@ -2,25 +2,16 @@ package com.caldoconf.service
 
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
-import io.ktor.auth.*
 import io.ktor.features.*
 import io.ktor.jackson.*
-import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-
-data class Conf(val title: String)
-
-var confs = listOf(Conf("1st Caldo"), Conf("2nd Caldo"))
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
-    install(Authentication) {
-    }
-
+fun Application.module() {
+    install(DefaultHeaders)
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
@@ -33,15 +24,7 @@ fun Application.module(testing: Boolean = false) {
         }
 
         route("/confs") {
-            get {
-                call.respond(mapOf("confs" to confs))
-            }
-
-            post {
-                val proposedConf = call.receive<Conf>()
-                confs += proposedConf
-                call.respond(mapOf("confs" to confs))
-            }
+            get {}
         }
     }
 }
